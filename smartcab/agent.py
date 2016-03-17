1762b13a31f1608acf1cd15dac0b97fa71cf50ea
Jou-ching Sung <georgesung@gmail.com>
2016-03-18 00:19:04 +0700

    debug

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -19,7 +19,7 @@ class LearningAgent(Agent):
         self.INITIAL_Q = 0.
         self.SIGMOID_OFFSET = 6
         self.SIGMOID_RATE = 0.05
-        self.MIN_RAND_PROB = 0.2
+        self.MIN_RAND_PROB = 0.1
         self.ALPHA = 1.
         self.GAMMA = 0.
 
@@ -59,26 +59,14 @@ class LearningAgent(Agent):
         # Execute action and get reward
         reward = self.env.act(self, action)
 
-        # DEBUG
-        if reward is 2:
-            print 'DEBUG: ' + str(state) + ', ' + str(action)
-            pass
-
         # TODO: Learn policy based on state, action, reward
-        '''
-        # Update the Q-value for the *previous* state, using current reward
-        if self.prev_sa is not None:
-            new_q = reward + self.GAMMA * max([self.qtable[self.compress_sa(state, a)] for a in valid_actions])
-            self.qtable[self.prev_sa] = (1 - self.ALPHA) * self.qtable[self.prev_sa] + self.ALPHA * new_q
-
-        self.prev_sa = self.compress_sa(state, action)
-        '''
         sa = self.compress_sa(state, action)
         new_q = reward + self.GAMMA * max([self.qtable[self.compress_sa(state, a)] for a in valid_actions])
         self.qtable[sa] = (1 - self.ALPHA) * self.qtable[sa] + self.ALPHA * new_q
 
         #print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
-        print 't = ' + str(t); print self.qtable
+        if t%10 == 0:  # [debug]
+            print 't = ' + str(t); print self.qtable  # [debug]
 
     def compress_sa(self, state, action):
         """Given state, action pair, compress it into a smaller representation space"""
@@ -117,11 +105,11 @@ def run():
     # Set up environment and agent
     e = Environment()  # create environment (also adds some dummy traffic)
     a = e.create_agent(LearningAgent)  # create agent
-    e.set_primary_agent(a, enforce_deadline=False)  # set agent to track
+    e.set_primary_agent(a, enforce_deadline=True)  # set agent to track
 
     # Now simulate it
-    sim = Simulator(e, update_delay=1.0)  # reduce update_delay to speed up simulation
-    sim.run(n_trials=10)  # press Esc or close pygame window to quit
+    sim = Simulator(e, update_delay=0.)  # reduce update_delay to speed up simulation
+    sim.run(n_trials=100)  # press Esc or close pygame window to quit
 
 
 if __name__ == '__main__':
--- smartcab/environment.py
+++ smartcab/environment.py
@@ -55,8 +55,7 @@ class Environment(object):
 
         # Dummy agents
         self.num_dummies = 3  # no. of dummy agents
-        # gsung debug
-        self.num_dummies = 10
+        #self.num_dummies = 10  # [debug]
         for i in xrange(self.num_dummies):
             self.create_agent(DummyAgent)
 
